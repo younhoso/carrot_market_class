7262c0e81ccb6184d7816a28e0b0fd372b728dd9
younhoso <thdbsgh3443@kakao.com>
2025-11-27 18:03:37 +0900

    Fea: 테일윈드 css 반영 작업

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={`bg-neutral-900 text-white max-w-screen-sm mx-auto`}
+    >
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
 export default function Home() {
-  return <main className="bg-black text-"></main>;
+  return (
+    <main className="bg-gray-300 h-screen flex items-center justify-center">
+      <div className="bg-white shadow-lg p-5 rounded-xl w-full">
+        <div>
+          <div className="flex flex-col">
+            <span className="text-gray-600 font-semibold -mb-1">
+              In transit
+            </span>
+            <span className="text-4xl font-semibold">Coolblue</span>
+          </div>
+          <div className="w-10 h-10 bg-orange-300" />
+        </div>
+        <div>
+          <span>Today</span>
+          <span>9:30-10:30</span>
+        </div>
+        <div>
+          <div />
+          <div />
+        </div>
+        <div>
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span>Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
 }
