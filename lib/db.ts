d65be1bcd5ee9ba9f94a7cade909f7b0f53cc9ed
younhoso <thdbsgh3443@kakao.com>
2025-12-31 11:03:09 +0900

    Feat: prisma onDelete

--- lib/db.ts
+++ lib/db.ts
@@ -2,10 +2,21 @@ import { PrismaClient } from "@prisma/client";
 export const db = new PrismaClient();
 
 async function test() {
-  const token = await db.sMSToken.findUnique({
-    where: {
-      id: 1,
+  const token = await db.sMSToken.create({
+    data: {
+      token: "121212",
+      user: {
+        connect: {
+          id: 1,
+        },
+      },
     },
+    // where: {
+    //   id: 1,
+    // },
+    // include: {
+    //   user: true,
+    // },
   });
 
   console.log(token);
